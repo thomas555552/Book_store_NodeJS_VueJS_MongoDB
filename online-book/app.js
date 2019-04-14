0fa5194a3717205a283bab01647a0bd174e4f0c3
thomas555552 <48090490+thomas555552@users.noreply.github.com>
2019-04-14 10:29:25 +0200

    Default project

---
+++ online-book/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var book = require('./routes/book');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/book', book);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// restful api error handler
+app.use(function(err, req, res, next) {
+  console.log(err);
+
+  if (req.app.get('env') !== 'development') {
+    delete err.stack;
+  }
+
+  res.status(err.statusCode || 500).json(err);
+});
+
+module.exports = app;
