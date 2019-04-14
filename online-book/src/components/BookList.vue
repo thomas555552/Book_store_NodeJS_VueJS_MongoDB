9aed037715eda8c6d2bdab2dff1f9851459a42f7
thomas555552 <48090490+thomas555552@users.noreply.github.com>
2019-04-14 10:58:25 +0200

    Default project

---
+++ online-book/src/components/BookList.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-row>
+    <b-col cols="12">
+      <h2>
+        Könyv Lista
+      </h2>
+      <b-table striped hover :items="books" :fields="fields">
+        <template slot="actions" scope="row">
+          <b-btn size="sm" @click.stop="details(row.item)">Details</b-btn>
+        </template>
+      </b-table>
+      <ul v-if="errors && errors.length">
+        <li v-for="error of errors">
+          {{error.message}}
+        </li>
+      </ul>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+
+  import axios from 'axios'
+
+  export default {
+    name: 'BookList',
+    data () {
+      return {
+        fields: {
+          isbn: { label: 'ISBN', sortable: true, 'class': 'text-center' },
+          title: { label: 'Cim', sortable: true },
+          author: { label: 'Szerzo', sortable: true },
+          published_year: { label: 'Kiadasi ev', sortable: true },
+          piece: { label: 'Darab'},
+          price: {label: 'Ar'},
+          actions: { label: 'Action', 'class': 'text-center' }
+        },
+        books: [],
+        errors: []
+      }
+    },
+    created () {
+      axios.get(`http://localhost:3000/book`)
+        .then(response => {
+          this.books = response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    },
+    methods: {
+      details (book) {
+        this.$router.push({
+          name: 'ShowBook',
+          params: { id: book._id }
+        })
+      }
+    }
+  }
+</script>
--- online-book/src/router/index.js
+++ online-book/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import BookList from '@/components/BookList'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+// import Cart from '@/components/Cart'
 
 Vue.use(Router)
 
@@ -8,8 +11,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'BookList',
+      component: BookList
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
